077863b009d8a5c41ed34c04c500d537106eb2d1
Xerkse <cjmp9876@gmail.com>
2024-03-05 16:31:54 -0600

    duno

--- main.sh
+++ main.sh
@@ -4,6 +4,8 @@ sudo sed -i "s/^  set timeout=5$/  set timeout=2/" /boot/grub/grub.cfg
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 sudo pacman -Sy --noconfirm sxiv mpv  \
+    pulsemixer \
+    torbrowser-launcher \
     lf ueberzug \
     zathura zathura-pdf-mupdf ffmpeg \
     imagemagick gimp \
@@ -12,9 +14,22 @@ sudo pacman -Sy --noconfirm sxiv mpv  \
     rsync \
     firefox \
     xcompmgr dunst \
-    vim neovim \
-    git \
+    vim neovim npm \
+    git github-cli\
+    man \
+    feh \
+    samba nfs-utils \
+    timeshift \
     libx11 libxft libxinerama xorg-xinit xorg-server webkit2gtk \
+    pass gnupg pass-otp \
+    bash-completion \
+    libreoffiec jre-openjdk \
+    newsboat \
+    ttf-joypixels \
+    noto-fonts-emoji \
+    exfat-utils \
+    xorg-xsetroot acpi \
+    maim \
     #mpd ncmpcpp 
 
 git clone --separate-git-dir="$HOME/.git" "https://github.com/Xerkse/configs" "tmpconfigs"
@@ -34,4 +49,11 @@ git clone --depth=1 https://github.com/Xerkse/my_dwm.git ~/.local/src/dwm \
 git clone --depth=1 https://github.com/Xerkse/my_sent.git ~/.local/src/sent \
     && sudo make -C ~/.local/src/sent install
 
+#install yay
+git clone https://aur.archlinux.org/yay.git
+cd yay && makepkg -si
+cd ~ && rm -r yay
 
+#not working, dont know why
+#yay -S ttf-ms-win10-auto
+yay -S espeak
