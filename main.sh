9473c9e73ec1b182a27cb8a8b930b1a46f992566
Xerkse <cjmp9876@gmail.com>
2025-04-28 08:40:18 -0500

    slock and dwn farbfield added

--- main.sh
+++ main.sh
@@ -1,47 +1,57 @@
+#!/bin/bash 
+
 sudo sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
 
 sudo sed -i "s/^  set timeout=5$/  set timeout=2/" /boot/grub/grub.cfg
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
-sudo pacman -Sy --noconfirm nsxiv mpv  \
-    pacman-contrib \
+sudo pacman -Sy --noconfirm \
     pulsemixer \
-    torbrowser-launcher \
+    nsxiv mpv  \
+    pacman-contrib \
     lf ueberzug \
     zathura zathura-pdf-mupdf ffmpeg \
     imagemagick gimp darktable\
-    perl-image-esiftool perl-archive-zip \
+    perl-image-exiftool perl-archive-zip \
     fzf xclip \
     zip unzip unrar p7zip \
     xdotool brightnessctl  \
     rsync samba nfs-utils \
-    firefox \
     xcompmgr dunst \
     vim neovim \
     npm \
     git github-cli\
-    man \
+    man-db \
     feh \
     timeshift cronie \
-    libx11 libxft libxinerama xorg-xinit xorg-xinput xorg-server webkit2gtk \
+    libx11 libxft libxinerama xorg-xinit xorg-xinput xorg-server webkit2gtk xorg-drivers\
     pass gnupg pass-otp \
     bash-completion \
-    libreoffiec jre-openjdk \
+    libreoffice-fresh jre-openjdk \
     newsboat \
-    ttf-joypixels \
     noto-fonts-emoji \
-    exfat-utils \
+    exfatprogs \
     xorg-xsetroot acpi \
     maim \
     wget \
     zbar \
+    base-devel \
     bc \
     zsh dash \
+    torbrowser-launcher firefox \
+    mpd ncmpcpp \
+    ttf-liberation-mono-nerd \
+    || exit
+
     #mpd ncmpcpp 
+    #ttf-joypixels 
+
+#change default shell
+chsh -s "$(which zsh)"
 
 git clone --separate-git-dir="$HOME/.git" "https://github.com/Xerkse/configs" "tmpconfigs"
 
-rsync --recursive --verbose --exclude '.git' "tmpconfigs/" "$HOME/" \
+rsync --links --recursive --verbose --exclude '.git' "tmpconfigs/" "$HOME/" \
     && rm -r tmpconfigs
 
 git clone --depth=1 https://github.com/Xerkse/my_dmenu.git ~/.local/src/dmenu \
@@ -55,6 +65,13 @@ git clone --depth=1 https://github.com/Xerkse/my_dwm.git ~/.local/src/dwm \
 
 git clone --depth=1 https://github.com/Xerkse/my_sent.git ~/.local/src/sent \
     && sudo make -C ~/.local/src/sent install
+
+git clone --depth=1 https://github.com/Xerkse/my_slock.git ~/.local/src/slock \
+    && sudo make -C ~/.local/src/slock install
+
+#suckless for sent
+git clone --depth=1 https://git.suckless.org/farbfeld ~/.local/src/farbfeld \
+    && sudo make -C ~/.local/src/farbfeld install
 
 #install yay
 git clone https://aur.archlinux.org/yay.git
@@ -73,3 +90,28 @@ sudo systemctl start cronie.service
 
 #use dash instead of bash for /bin/sh
 sudo ln -sf /bin/dash /bin/sh
+
+#arkenfox userjs
+wget https://raw.github.com/arkenfox/user.js/master/user.js
+wget https://raw.github.com/arkenfox/user.js/master/updater.sh
+wget https://raw.github.com/arkenfox/user.js/master/prefsCleaner.sh
+sudo chmod +x updater.sh
+sudo chmod +x prefsCleaner.sh
+mv user.js "$HOME/.config/areknfox/"
+mv updater.sh "$HOME/.config/areknfox/"
+mv prefsCleaner.sh  "$HOME/.config/areknfox/"
+
+#run firefox to get the profiles file loaded so it can be edited
+echo -n "firefox will be killed. Hit Enter." && read
+pkill firefox
+firefox -CreateProfile "arkenfox $HOME/.config/arkenfox"
+
+#change arkenfox to default
+echo -n "Firefox will be started. Make arkenfox the default profile. Hit Enter." && read
+pkill firefox
+firefox --new-window about:profiles || exit
+
+echo -n "install ublock origin then close. Hit Enter." && read
+pkill firefox
+firefox "https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/?utm_source=addons.mozilla.org"
+
