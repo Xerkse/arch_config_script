b3290b651c6bf655e7056102be3f8acc85c3c67e
Xerkse <cjmp9876@gmail.com>
2025-05-04 22:50:49 -0500

    ability to update suckless and configs

--- main.sh
+++ main.sh
@@ -2,8 +2,7 @@
 
 sudo sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
 
-sudo sed -i "s/^  set timeout=5$/  set timeout=2/" /boot/grub/grub.cfg
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+sudo sed -i "s/^  set timeout=5$/  set timeout=2/" /boot/grub/grub.cfg && sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 sudo pacman -Sy --noconfirm \
     pulsemixer \
@@ -19,6 +18,7 @@ sudo pacman -Sy --noconfirm \
     rsync samba nfs-utils \
     xcompmgr dunst \
     vim neovim \
+    tree-sitter-cli \
     npm \
     git github-cli\
     man-db \
@@ -49,40 +49,123 @@ sudo pacman -Sy --noconfirm \
 #change default shell
 chsh -s "$(which zsh)"
 
-git clone --separate-git-dir="$HOME/.git" "https://github.com/Xerkse/configs" "tmpconfigs"
+update_git() {
+    dir_path="$1"
 
-rsync --links --recursive --verbose --exclude '.git' "tmpconfigs/" "$HOME/" \
-    && rm -r tmpconfigs
+    if [ -d "$dir_path/.git" ]; then
+        git --git-dir="$dir_path/.git" --work-tree="$dir_path" pull && return 0
+        printf "unable to easily update %s would you like to force it?[y/N]" "$dir_path"
+        read -r force_input
+        case "$force_input" in
+            [Yy]* ) {
+                git --git-dir="$dir_path/.git" --work-tree="$dir_path" reset --hard
+                git --git-dir="$dir_path/.git" --work-tree="$dir_path" pull && echo "$dir_path successfully updated"
+                return 0
+            } ;;
+            [Nn]* ) return 0 ;;
+            *) echo "this is bad -> $force_input?" & exit 1 ;;
+        esac
+    else
+        return 1
+    fi
+}
 
-git clone --depth=1 https://github.com/Xerkse/my_dmenu.git ~/.local/src/dmenu \
-    && sudo make -C ~/.local/src/dmenu install
+# git clone --separate-git-dir="$HOME/.git" "https://github.com/Xerkse/configs" "tmpconfigs"
+#
+# rsync --links --recursive --verbose --exclude '.git' "tmpconfigs/" "$HOME/" \
+#     && rm -r tmpconfigs
 
-git clone --depth=1 https://github.com/Xerkse/my_st.git ~/.local/src/st \
-    && sudo make -C ~/.local/src/st install
+configs_path="$HOME/.git"
+if update_git "$configs_path"; then
+    pass
+else
+    git clone --separate-git-dir="$HOME/.git" "https://github.com/Xerkse/configs" "tmpconfigs"
+    rsync --links --recursive --verbose --exclude '.git' "tmpconfigs/" "$HOME/" \
+        && rm -r tmpconfigs
+fi
 
-git clone --depth=1 https://github.com/Xerkse/my_dwm.git ~/.local/src/dwm \
-    && sudo make -C ~/.local/src/dwm install
+mkdir "$HOME/.trash"
 
-git clone --depth=1 https://github.com/Xerkse/my_sent.git ~/.local/src/sent \
-    && sudo make -C ~/.local/src/sent install
 
-git clone --depth=1 https://github.com/Xerkse/my_slock.git ~/.local/src/slock \
-    && sudo make -C ~/.local/src/slock install
+dmenu_path="$HOME/.local/src/dmenu/"
+if update_git "$dmenu_path"; then
+    sudo make -C "$dmenu_path" install
+else
+    git clone --depth=1 "https://github.com/Xerkse/my_dmenu.git" "$HOME/.local/src/dmenu" \
+    && sudo make -C "$HOME/.local/src/dmenu" install
+fi
+
+st_path="$HOME/.local/src/st/"
+if update_git "$st_path"; then
+    sudo make -C "$st_path" install
+else
+    git clone --depth=1 "https://github.com/Xerkse/my_st.git" "$HOME/.local/src/st" \
+    && sudo make -C "$HOME/.local/src/st" install
+fi
+
+dwm_path="$HOME/.local/src/dwm/"
+if update_git "$dwm_path"; then
+    sudo make -C "$dwm_path" install
+else
+    git clone --depth=1 "https://github.com/Xerkse/my_dwm.git" "$HOME/.local/src/dwm" \
+    && sudo make -C "$HOME/.local/src/dwm" install
+fi
+
+sent_path="$HOME/.local/src/sent/"
+if update_git "$sent_path"; then
+    sudo make -C "$sent_path" install
+else
+    git clone --depth=1 "https://github.com/Xerkse/my_sent.git" "$HOME/.local/src/sent" \
+    && sudo make -C "$HOME/.local/src/sent" install
+fi
+
+slock_path="$HOME/.local/src/slock/"
+if update_git "$slock_path"; then
+    sudo make -C "$slock_path" install
+else
+    git clone --depth=1 "https://github.com/Xerkse/my_slock.git" "$HOME/.local/src/slock" \
+    && sudo make -C "$HOME/.local/src/slock" install
+fi
+
+farbfeld_path="$HOME/.local/src/farbfeld/"
+if update_git "$farbfeld_path"; then
+    sudo make -C "$farbfeld_path" install
+else
+    git clone --depth=1 "https://git.suckless.org/farbfeld" "$HOME/.local/src/farbfeld" \
+        && sudo make -C "$HOME/.local/src/farbfeld" install
+fi
+
+
+
+# git clone --depth=1 "https://github.com/Xerkse/my_st.git" "$HOME/.local/src/st" \
+#     && sudo make -C "$HOME/.local/src/st" install
+#
+# git clone --depth=1 "https://github.com/Xerkse/my_dwm.git" "$HOME/.local/src/dwm" \
+#     && sudo make -C "$HOME/.local/src/dwm" install
+#
+# git clone --depth=1 "https://github.com/Xerkse/my_sent.git" "$HOME/.local/src/sent" \
+#     && sudo make -C "$HOME/.local/src/sent" install
+#
+# git clone --depth=1 "https://github.com/Xerkse/my_slock.git" "$HOME/.local/src/slock" \
+#     && sudo make -C "$HOME/.local/src/slock" install
 
 #suckless for sent
-git clone --depth=1 https://git.suckless.org/farbfeld ~/.local/src/farbfeld \
-    && sudo make -C ~/.local/src/farbfeld install
+# git clone --depth=1 "https://git.suckless.org/farbfeld" "$HOME/.local/src/farbfeld" \
+#     && sudo make -C "$HOME/.local/src/farbfeld" install
 
 #install yay
 git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -si
-cd ~ && rm -r yay
+cd "$HOME" && sudo rm -r yay
 
 #not working, dont know why
 #yay -S ttf-ms-win10-auto
 yay -S espeak \
     simple-mtpfs \
 
+# slock service
+ln -s "$HOME/.config/x11/services/slock@.service" "/etc/systemd/system/"
+systemctl enable slock@xerkse
 
 #timeshift
 sudo systemctl enable cronie.service
@@ -97,21 +180,22 @@ wget https://raw.github.com/arkenfox/user.js/master/updater.sh
 wget https://raw.github.com/arkenfox/user.js/master/prefsCleaner.sh
 sudo chmod +x updater.sh
 sudo chmod +x prefsCleaner.sh
-mv user.js "$HOME/.config/areknfox/"
-mv updater.sh "$HOME/.config/areknfox/"
-mv prefsCleaner.sh  "$HOME/.config/areknfox/"
+mkdir "$HOME/.config/arkenfox"
+mv user.js "$HOME/.config/arkenfox/"
+mv updater.sh "$HOME/.config/arkenfox/"
+mv prefsCleaner.sh  "$HOME/.config/arkenfox/"
 
 #run firefox to get the profiles file loaded so it can be edited
-echo -n "firefox will be killed. Hit Enter." && read
+echo -n "firefox will be killed. Hit Enter." && read -r
 pkill firefox
 firefox -CreateProfile "arkenfox $HOME/.config/arkenfox"
 
 #change arkenfox to default
-echo -n "Firefox will be started. Make arkenfox the default profile. Hit Enter." && read
+echo -n "Firefox will be started. Make arkenfox the default profile. Hit Enter." && read -r
 pkill firefox
 firefox --new-window about:profiles || exit
 
-echo -n "install ublock origin then close. Hit Enter." && read
+echo -n "install ublock origin then close. Hit Enter." && read -r
 pkill firefox
 firefox "https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/?utm_source=addons.mozilla.org"
 
